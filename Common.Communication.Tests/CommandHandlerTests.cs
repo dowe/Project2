717aef17361f89097b72bb9eae5cfd1ac8635d72
oberger4711 <oberger@stud.hs-offenburg.de>
2015-04-26 19:12:52 +0200

    Added unit test for CommandHandler.
    - Added test for AcceptedType of CommandHandler.

---
+++ Common.Communication.Tests/CommandHandlerTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Common.Communication.Tests
+{
+    [TestClass]
+    public class CommandHandlerTests
+    {
+
+        private TesteeCommandHandler CreateTesteeCommandHandler()
+        {
+            return new TesteeCommandHandler();
+        }
+
+        [TestMethod]
+        public void Test_AcceptedType_OnCall_ReturnsGenericType()
+        {
+            TesteeCommandHandler testee = CreateTesteeCommandHandler();
+
+            Assert.AreEqual(typeof (DummyCommand), testee.AcceptedType);
+        }
+
+        private class DummyCommand : Command
+        {
+        }
+
+        private class TesteeCommandHandler : CommandHandler<DummyCommand>
+        {
+
+            public bool HandledCommand { get; private set; }
+
+            protected override void Handle(DummyCommand command, string connectionIdOrNull)
+            {
+                HandledCommand = true;
+            }
+
+        }
+
+    }
+}
