2b76b1f22f96ed8dbc66ee7d36d1aa6965a5dad7
oberger4711 <oberger@stud.hs-offenburg.de>
2015-04-26 19:38:29 +0200

    Added best case test for WaitForResponse method.
    - Added test for WaitForResponse after having received a matching command.

---
+++ Common.Communication.Tests/ResponseCommandHandlerTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Common.Communication.Tests
+{
+    [TestClass]
+    public class ResponseCommandHandlerTests
+    {
+
+        private ResponseCommandHandler<DummyCommand> CreateTestee(Guid id)
+        {
+            return new ResponseCommandHandler<DummyCommand>(id);
+        }
+        
+        [TestMethod]
+        public void Test_WaitForResponse_AfterReceivingMatchingResponse_ReturnsResponse()
+        {
+            var acceptedId = Guid.NewGuid();
+            ResponseCommandHandler<DummyCommand> testee = CreateTestee(acceptedId);
+            var dummyCommand = new DummyCommand(acceptedId);
+
+            testee.TryHandleCommand(dummyCommand, "Look at me I am a connectionId.");
+            var receivedCommand = testee.WaitForResponse(0);
+
+            Assert.AreEqual(dummyCommand, receivedCommand);
+        }
+
+        private class DummyCommand : Command
+        {
+            public DummyCommand(Guid id) : base(id)
+            {
+            }
+        }
+
+    }
+}
