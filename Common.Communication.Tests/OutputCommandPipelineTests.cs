97518a231f700f1e9d58d851f963a18fe1e0419f
oberger4711 <oberger@stud.hs-offenburg.de>
2015-04-26 18:48:37 +0200

    Added unit test for output pipeline.
    - Added test for Sending a command.

---
+++ Common.Communication.Tests/OutputCommandPipelineTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+
+namespace Common.Communication.Tests
+{
+    [TestClass]
+    public class OutputCommandPipelineTests
+    {
+
+        private OutputCommandPipeline CreateTestee(ICommandSerializer serializer)
+        {
+            return new OutputCommandPipeline(serializer);
+        }
+
+        [TestMethod]
+        public void Test_SendSendable_CallsSendWithSerializedCommand()
+        {
+            bool sentSerializedDummy = false;
+            var command = new DummyCommand();
+            var serializedCommand = "Look at me I am serialized.";
+            Sendable<Command> sendableDummy = new Sendable<Command>(command, s =>
+            {
+                if (s.Equals(serializedCommand)) sentSerializedDummy = true;
+            });
+            var fakeSerializer = Substitute.For<ICommandSerializer>();
+            fakeSerializer.SerializeCommand(command).Returns(serializedCommand);
+            var testee = CreateTestee(fakeSerializer);
+            testee.Start();
+
+            testee.Send(sendableDummy);
+
+            testee.Stop();
+
+            Assert.IsTrue(sentSerializedDummy);
+        }
+
+        private class DummyCommand : Command
+        {
+        }
+    }
+}
