8a222f17d13bc9688650872d3f2b850bea7a1565
oberger4711 <oberger@stud.hs-offenburg.de>
2015-05-02 11:21:40 +0200

    Added smartphone solution with driver app.
    - Driver app contains a pretty ugly XAML UI for the login.
    - Solution is separated from the Project2 sln because the Project2
      solution cannot be built anymore without having Xamarin Visual Studio
      support...
    - If Visual Studio support is available in the future the Smartphone
      solution might be integrated into Project2 sln.

---
+++ Smartphone/Smartphone.Driver/Smartphone.Driver.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Common.Communication.Client;
+using Common.Commands;
+using Common.DataTransferObjects;
+
+namespace Smartphone.Driver
+{
+	public class App : Application
+	{
+
+		public App ()
+		{
+			MainPage = new LoginPage();
+		}
+
+		protected override void OnStart ()
+		{
+
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
+
