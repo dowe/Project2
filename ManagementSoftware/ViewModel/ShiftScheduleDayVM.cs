f4c0e88068fab3089aaa5e8e6b1a2c8595826cee
D Fritsch <danielw.fritsch@web.de>
2015-05-03 04:28:05 +0200

    ShiftSchedule started
    - switch of Month and Day View implemented
    - ViewModel for Month and Day View started
    - common ShiftSchedule Model of Month and Day View implemented

---
+++ ManagementSoftware/ViewModel/ShiftScheduleDayVM.cs
@@ -0,0 +1,37 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using ManagementSoftware.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ManagementSoftware.ViewModel
+{
+    public class ShiftScheduleDayVM : ViewModelBase
+    {
+        private ShiftScheduleRawModel _ShiftScheduleRawModel;
+        private RelayCommand _SwitchToMonthCommand;
+
+        public ShiftScheduleDayVM(ShiftScheduleRawModel _ShiftScheduleRawModel, RelayCommand _SwitchToMonthCommand)
+        {
+            this._ShiftScheduleRawModel = _ShiftScheduleRawModel;
+            this._SwitchToMonthCommand = _SwitchToMonthCommand;
+
+            _ShiftScheduleRawModel.Change += RawModelChanged;
+        }
+
+        private void RawModelChanged(object sender, ShiftScheduleRawModelChangeEventArgs e)
+        {
+            //TODO
+        }
+
+        public RelayCommand SwitchToMonthCommand
+        {
+            get
+            {
+                return _SwitchToMonthCommand;
+            }
+        }
+    }
+}
---
+++ ManagementSoftware/ViewModel/ShiftScheduleVM.cs
@@ -0,0 +1,71 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using ManagementSoftware.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagementSoftware.ViewModel
+{
+    public class ShiftScheduleVM : ViewModelBase
+    {
+        private ViewModelBase _CurrentViewModel;
+
+        private ShiftScheduleMonthVM _ShiftScheduleMonthVM;
+        private ShiftScheduleDayVM _ShiftScheduleDayVM;
+
+        public ShiftScheduleVM()
+        {
+            ShiftScheduleRawModel _ShiftScheduleRawModel = new ShiftScheduleRawModel();
+
+            RelayCommand _SwitchToMonthCommand = new RelayCommand(() => SwitchToShiftScheduleMonthVM());
+            RelayCommand _SwitchToDayCommand = new RelayCommand(() => SwitchToShiftScheduleDayVM());
+
+            _ShiftScheduleMonthVM = new ShiftScheduleMonthVM(_ShiftScheduleRawModel, _SwitchToDayCommand);
+            _ShiftScheduleDayVM = new ShiftScheduleDayVM(_ShiftScheduleRawModel, _SwitchToMonthCommand);
+
+            _CurrentViewModel = _ShiftScheduleMonthVM;
+        }
+
+        public ViewModelBase ShiftScheduleMonthVM
+        {
+            get
+            {
+                return _ShiftScheduleMonthVM;
+            }
+        }
+
+        public ViewModelBase ShiftScheduleDayVM
+        {
+            get
+            {
+                return _ShiftScheduleDayVM;
+            }
+        }
+
+        private void SwitchToShiftScheduleMonthVM()
+        {
+            CurrentViewModel = _ShiftScheduleMonthVM;
+        }
+
+        private void SwitchToShiftScheduleDayVM()
+        {
+            CurrentViewModel = _ShiftScheduleDayVM;
+        }
+
+        public ViewModelBase CurrentViewModel
+        {
+            get
+            {
+                return _CurrentViewModel;
+            }
+            set
+            {
+                _CurrentViewModel = value;
+                RaisePropertyChanged();
+            }
+        }
+    }
+}
