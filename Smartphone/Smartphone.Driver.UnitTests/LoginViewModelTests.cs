fc45a27ffb51bc2742a6927574f83aff59ebc62e
oberger4711 <oberger@stud.hs-offenburg.de>
2015-05-05 22:19:11 +0200

    Added test project.

---
+++ Smartphone/Smartphone.Driver.UnitTests/LoginViewModelTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System;
+using NSubstitute;
+
+namespace Smartphone.Driver.UnitTests
+{
+	[TestFixture ()]
+	public class LoginViewModelTests
+	{
+		[Test ()]
+		public void Login_WhenNotYetConnected_Connects ()
+		{
+			// TODO
+		}
+	}
+}
+
