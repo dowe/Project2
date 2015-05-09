96cbec77fe9fb5c780ced64a01a82f200b6dffcf
oberger4711 <oberger@stud.hs-offenburg.de>
2015-05-10 00:06:06 +0200

    Added input field for end km.

--- Smartphone/Smartphone.Driver/ViewModels/LogoutViewModel.cs
+++ Smartphone/Smartphone.Driver/ViewModels/LogoutViewModel.cs
@@ -1,13 +1,61 @@
 ﻿using System;
 using GalaSoft.MvvmLight;
+using Common.Communication.Client;
 
 namespace Smartphone.Driver
 {
 	public class LogoutViewModel : ViewModelBase
 	{
-		public LogoutViewModel ()
+
+		private const string EndKmProperty = "EndKm";
+		private const string IsCommunicatingProperty = "IsCommunicating";
+		private const string IsNotCommunicatingProperty = "IsNotCommunicating";
+
+		private IClientConnection connection = null;
+		private float endKm = 3;
+		private bool isCommunicating = false;
+
+		public LogoutViewModel (IClientConnection connection)
 		{
+			this.connection = connection;
 		}
+
+		public float EndKm
+		{
+			get {
+				return endKm;
+			}
+			set {
+				if (Math.Abs (value - endKm) > float.Epsilon)
+				{
+					endKm = value;
+					RaisePropertyChanged (EndKmProperty);
+				}
+			}
+		}
+
+		public bool IsCommunicating
+		{
+			get {
+				return isCommunicating;
+			}
+			set {
+				if (value != isCommunicating)
+				{
+					isCommunicating = value;
+					RaisePropertyChanged (IsCommunicatingProperty);
+					RaisePropertyChanged (IsNotCommunicatingProperty);
+				}
+			}
+		}
+
+		public bool IsNotCommunicating
+		{
+			get {
+				return !IsCommunicating;
+			}
+		}
+
 	}
 }
 
