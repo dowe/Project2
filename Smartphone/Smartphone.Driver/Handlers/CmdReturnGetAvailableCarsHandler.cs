58b8a7ee03d708793d3d6ae6655ec5fb29ae65c4
oberger4711 <oberger@stud.hs-offenburg.de>
2015-05-15 12:34:31 +0200

    Moved handlers into new folder handlers.

---
+++ Smartphone/Smartphone.Driver/Handlers/CmdReturnGetAvailableCarsHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Common.Commands;
+using Common.Communication;
+
+namespace Smartphone.Driver
+{
+	public class CmdReturnGetAvailableCarsHandler : CommandHandler<CmdReturnGetAvailableCars>
+	{
+
+		private WrappedCars cars = null;
+
+		public CmdReturnGetAvailableCarsHandler (WrappedCars cars)
+		{
+			this.cars = cars;
+		}
+			
+		protected override void Handle (CmdReturnGetAvailableCars command, string connectionIdOrNull)
+		{
+			// Update Available cars model.
+			Xamarin.Forms.Device.BeginInvokeOnMainThread (() => cars.UpdateAll (command.AvailableCars));
+		}
+
+	}
+}
+
