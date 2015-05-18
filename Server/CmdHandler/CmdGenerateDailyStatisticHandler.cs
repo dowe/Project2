44394c88b898c5e22e895d8439fa6610c2fc2609
Daniel Friedel <daniel.s.friedel@gmail.com>
2015-05-18 13:15:25 +0200

    nullable fix

--- Server/CmdHandler/CmdGenerateDailyStatisticHandler.cs
+++ Server/CmdHandler/CmdGenerateDailyStatisticHandler.cs
@@ -27,35 +27,38 @@ namespace Server.CmdHandler
             this.connection = connection;
             this.db = db;
             this.data = data;
+            ds = new DailyStatistic();
         }
 
         protected override void Handle(CmdGenerateDailyStatistic command, string connectionIdOrNull)
         {
+          
             db.StartTransaction();
             _OrderList = db.GetAllOrders(null);
             db.EndTransaction(TransactionEndOperation.READONLY);
-
+            DateTime now = DateTime.Now;
             foreach (Order o in _OrderList)
             {
+                
                 //Alles für Orders
                 //falls in den letzten 24h bestellt wurde
-                if (o.OrderDate != null && (o.OrderDate - DateTime.Now).TotalHours < 24)
+                if (o.OrderDate != null && (o.OrderDate> now.AddHours(-24) && o.OrderDate <=now ))
                     ds.NumberOfOrderedOrders++;
                 //falls in den letzten 24h beendet wurde
-                if (o.CompleteDate != null && (o.CompleteDate - DateTime.Now).TotalHours < 24)
+                if (o.CompleteDate != null && (o.CompleteDate> now.AddHours(-24) && o.CompleteDate <=now ))
                     ds.NumberOfCompletedOrders++;
                 //sonst falls in progress
-                else if (o.BringDate != null && (o.BringDate - DateTime.Now).TotalHours < 24)
+                else if (o.BringDate != null && (o.BringDate> now.AddHours(-24) && o.BringDate <=now ))
                     ds.NumberOfOrdersInProgress++;
 
                 //Alles für tests
                 foreach (Test t in o.Test)
                 {
                     //in letzten 24h gestartet aber noch nicht zu ende
-                    if (t.StartDate != null && (t.StartDate - DateTime.Now).TotalHours < 24 && t.EndDate == null)
+                    if (t.StartDate != null && (t.StartDate> now.AddHours(-24) && t.StartDate <= now ) && t.EndDate == null)
                         ds.NumberOfTestsInProgress++;
                     //in letzten 24h beendet
-                    else if (t.EndDate != null && (t.EndDate - DateTime.Now).TotalHours < 24)
+                    else if (t.EndDate != null && (t.EndDate> now.AddHours(-24) && t.EndDate <= now))
                         ds.NumberOfCompletedTests++;
                 }
 
