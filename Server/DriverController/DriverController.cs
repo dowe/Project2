66ed181f6a3b67510038872f4aab5e8ecc317cf6
oberger4711 <oberger@stud.hs-offenburg.de>
2015-05-18 23:16:42 +0200

    Added default ctor with preset constraints.

--- Server/DriverController/DriverController.cs
+++ Server/DriverController/DriverController.cs
@@ -10,10 +10,23 @@ namespace Server.DriverController
         private OptionsCalculator routeCalculator = null;
         private OptionsEvaluator evaluator = null;
 
+        /// <summary>
+        /// This ctor is used for testing purpose (dependency injection).
+        /// </summary>
+        /// <param name="routeCalculator"></param>
+        /// <param name="evaluator"></param>
         public DriverController(OptionsCalculator routeCalculator, OptionsEvaluator evaluator)
         {
             this.routeCalculator = routeCalculator;
             this.evaluator = evaluator;
+        }
+
+        public DriverController()
+            : this(new OptionsCalculator(new DirectionsRouteDistanceCalculator(false)), new OptionsEvaluator())
+        {
+            evaluator.AddHardConstraint(o => o.TotalLeftDistance.Time < 6); // All orders must be finished before 6h passed.
+            // TODO Add shift time hard constraint.
+            evaluator.SetSoftConstraint(o => -o.TotalLeftDistance.Time); // The faster, the better.
         }
 
         public Driver DetermineDriverOrNull(IEnumerable<Driver> allDrivers, IEnumerable<Order> allUnfinishedOrders, Address destination)
