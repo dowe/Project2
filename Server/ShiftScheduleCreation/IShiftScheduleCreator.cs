87d9e375ff26ad81aa3aef8fd74b177cbe2aa0d7
unknown <m.wacker@sticking.ag>
2015-05-22 01:42:34 +0200

    Added and Implemented ShiftScheduleCreator

---
+++ Server/ShiftScheduleCreation/IShiftScheduleCreator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.DataTransferObjects;
+
+namespace Server.ShiftScheduleCreation
+{
+    public interface IShiftScheduleCreator
+    {
+        ShiftSchedule createShiftSchedule(ShiftSchedule last, DateTime date);
+    }
+}
