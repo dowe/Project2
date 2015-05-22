7b86d035beeae7dc3793277f57c6f2ed14943ac2
oberger4711 <oberger@stud.hs-offenburg.de>
2015-05-22 13:05:48 +0200

    Moved notification texts to a static class.

---
+++ Smartphone/Smartphone.Driver/Const/NotificationTexts.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Smartphone.Driver.Const
+{
+	public static class NotificationTexts
+	{
+
+		public const string ORDER_REMINDER_TEXT = "Only one hour left to collect the samples. Hurry up!";
+		public static string OrderReminderTitle(long orderID)
+		{
+			return "Order " + orderID;
+		}
+
+	}
+}
+
