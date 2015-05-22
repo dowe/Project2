cbe5e23e4438b8045daea81cc1382a8d6564fea9
oberger4711 <oberger@stud.hs-offenburg.de>
2015-05-22 12:43:19 +0200

    Added toasting to select car view.

--- Smartphone/Smartphone.Driver/Const/ToastTexts.cs
+++ Smartphone/Smartphone.Driver/Const/ToastTexts.cs
@@ -9,6 +9,7 @@ namespace Smartphone.Driver.Const
 		public const string FAILED_LOGOUT_ORDERS_LEFT = "You still have orders left.";
 		public const string FAILED_LAUNCH_MAP = "Could not start the map. Make sure you have Google Maps installed.";
 		public const string FAILED_SET_COLLECTED = "Set collected failed.";
+		public const string FAILED_SELECT_CAR = "Select car failed.";
 		public const string SERVER_NO_ANSWER = "Server did not answer.";
 		public static string AlreadyAssignedToCar(string carID)
 		{
