d4751a7d0848762720193792ce4daefbd7a8d518
oberger4711 <oberger@stud.hs-offenburg.de>
2015-06-03 00:42:22 +0200

    Formatted in VS.

--- Smartphone/Smartphone.Driver/ViewModels/LoginViewModel.cs
+++ Smartphone/Smartphone.Driver/ViewModels/LoginViewModel.cs
@@ -14,163 +14,168 @@ using Smartphone.Driver.Const;
 
 namespace Smartphone.Driver.ViewModels
 {
-	public class LoginViewModel : ViewModelBase
-	{
+    public class LoginViewModel : ViewModelBase
+    {
 
-		public const string UsernameProperty = "Username";
-		public const string PasswordProperty = "Password";
-		public const string CanConnectProperty = "CanConnect";
-		public const string IsCommunicatingProperty = "IsCommunicating";
-		public const string IsNotCommunicatingProperty = "IsNotCommunicating";
+        public const string UsernameProperty = "Username";
+        public const string PasswordProperty = "Password";
+        public const string CanConnectProperty = "CanConnect";
+        public const string IsCommunicatingProperty = "IsCommunicating";
+        public const string IsNotCommunicatingProperty = "IsNotCommunicating";
 
-		private IClientConnection connection = null;
-		private Session session = null;
-		private IToaster toaster = null;
-	    private GPSPositionSender positionSender = null;
+        private IClientConnection connection = null;
+        private Session session = null;
+        private IToaster toaster = null;
+        private GPSPositionSender positionSender = null;
 
-		private string username = null;
-		private string password = null;
-		private bool communicating = false;
-		private RelayCommand loginCommand = null;
+        private string username = null;
+        private string password = null;
+        private bool communicating = false;
+        private RelayCommand loginCommand = null;
 
-		public LoginViewModel (IClientConnection connection, Session session, IToaster toaster, GPSPositionSender positionSender)
-		{
-			this.connection = connection;
-			this.session = session;
-			this.toaster = toaster;
-		    this.positionSender = positionSender;
+        public LoginViewModel(IClientConnection connection, Session session, IToaster toaster, GPSPositionSender positionSender)
+        {
+            this.connection = connection;
+            this.session = session;
+            this.toaster = toaster;
+            this.positionSender = positionSender;
 
-			username = "Driv3";
-			password = "Driv3";
-			communicating = false;
-		}
+            username = "Driv3";
+            password = "Driv3";
+            communicating = false;
+        }
 
-		public string Username
-		{
-			get { return username; }
-			set {
-				if (!string.Equals (username, value))
-				{
-					username = value;
-					RaisePropertyChanged (UsernameProperty);
-				}
-			}
-		}
+        public string Username
+        {
+            get { return username; }
+            set
+            {
+                if (!string.Equals(username, value))
+                {
+                    username = value;
+                    RaisePropertyChanged(UsernameProperty);
+                }
+            }
+        }
 
-		public string Password
-		{
-			get { return password; }
-			set {
-				if (!string.Equals (password, value))
-				{
-					password = value;
-					RaisePropertyChanged (PasswordProperty);
-				}
-			}
-		}
+        public string Password
+        {
+            get { return password; }
+            set
+            {
+                if (!string.Equals(password, value))
+                {
+                    password = value;
+                    RaisePropertyChanged(PasswordProperty);
+                }
+            }
+        }
 
-		public bool IsCommunicating
-		{
-			get { return communicating; }
-			set {
-				if (communicating != value)
-				{
-					communicating = value;
-					RaisePropertyChanged (IsCommunicatingProperty);
-					RaisePropertyChanged (IsNotCommunicatingProperty);
-				}
-			}
-		}
+        public bool IsCommunicating
+        {
+            get { return communicating; }
+            set
+            {
+                if (communicating != value)
+                {
+                    communicating = value;
+                    RaisePropertyChanged(IsCommunicatingProperty);
+                    RaisePropertyChanged(IsNotCommunicatingProperty);
+                }
+            }
+        }
 
-		public bool IsNotCommunicating
-		{
-			get { return !IsCommunicating; }
-		}
+        public bool IsNotCommunicating
+        {
+            get { return !IsCommunicating; }
+        }
 
-		public RelayCommand LoginCommand
-		{
-			get {
-				return loginCommand ?? (loginCommand = new RelayCommand (Login));
-			}
-		}
+        public RelayCommand LoginCommand
+        {
+            get
+            {
+                return loginCommand ?? (loginCommand = new RelayCommand(Login));
+            }
+        }
 
-		private async void Login()
-		{
-			IsCommunicating = true;
-			if (!connection.ConnectionState.Equals(ConnectionState.Connected))
-			{
-				await Connect ();
-			}
-			if (connection.ConnectionState.Equals (ConnectionState.Connected))
-			{
-				var cmdLogin = new CmdLoginDriver (username, password);
-				var response = connection.SendWait<CmdReturnLoginDriver> (cmdLogin);
-				if (response != null)
-				{
-					if (response.Success)
-					{
-						OnLoginSuccessful (response.AssignedCarIDOrNull);
-					} else
-					{
-						toaster.MakeToast (ToastTexts.FAILED_LOGIN);
-					}
-				}
-				else
-				{
-					toaster.MakeToast (ToastTexts.SERVER_NO_ANSWER);
-				}
-			}
-			else
-			{
-				toaster.MakeToast (ToastTexts.SERVER_NO_ANSWER);
-			}
-			IsCommunicating = false;
-		}
+        private async void Login()
+        {
+            IsCommunicating = true;
+            if (!connection.ConnectionState.Equals(ConnectionState.Connected))
+            {
+                await Connect();
+            }
+            if (connection.ConnectionState.Equals(ConnectionState.Connected))
+            {
+                var cmdLogin = new CmdLoginDriver(username, password);
+                var response = connection.SendWait<CmdReturnLoginDriver>(cmdLogin);
+                if (response != null)
+                {
+                    if (response.Success)
+                    {
+                        OnLoginSuccessful(response.AssignedCarIDOrNull);
+                    }
+                    else
+                    {
+                        toaster.MakeToast(ToastTexts.FAILED_LOGIN);
+                    }
+                }
+                else
+                {
+                    toaster.MakeToast(ToastTexts.SERVER_NO_ANSWER);
+                }
+            }
+            else
+            {
+                toaster.MakeToast(ToastTexts.SERVER_NO_ANSWER);
+            }
+            IsCommunicating = false;
+        }
 
-		private Task<bool> Connect()
-		{
-			return Task.Run (() =>
-			{
-				bool success = false;
-				try
-				{
-					connection.Connect ();
-					success = true;
-				}
-				catch (ConnectionException)
-				{
-					success = false;
-				}
+        private Task<bool> Connect()
+        {
+            return Task.Run(() =>
+            {
+                bool success = false;
+                try
+                {
+                    connection.Connect();
+                    success = true;
+                }
+                catch (ConnectionException)
+                {
+                    success = false;
+                }
 
-				return success;
-			});
-		}
+                return success;
+            });
+        }
 
-		private void OnLoginSuccessful(string assignedCarIDOrNull)
-		{
-			session.Username = username;
-			if (assignedCarIDOrNull != null)
-			{
-				// Driver already has a car assigned. Skip the car selection.
-				session.CarID = assignedCarIDOrNull;
-				toaster.MakeToast (ToastTexts.AlreadyAssignedToCar(assignedCarIDOrNull));
+        private void OnLoginSuccessful(string assignedCarIDOrNull)
+        {
+            session.Username = username;
+            if (assignedCarIDOrNull != null)
+            {
+                // Driver already has a car assigned. Skip the car selection.
+                session.CarID = assignedCarIDOrNull;
+                toaster.MakeToast(ToastTexts.AlreadyAssignedToCar(assignedCarIDOrNull));
 
-				CmdGetDriversUnfinishedOrders cmdGetOrders = new CmdGetDriversUnfinishedOrders (session.Username);
-				connection.Send (cmdGetOrders);
+                CmdGetDriversUnfinishedOrders cmdGetOrders = new CmdGetDriversUnfinishedOrders(session.Username);
+                connection.Send(cmdGetOrders);
 
                 positionSender.Start();
 
-				Messenger.Default.Send<MsgSwitchOrdersPage> (new MsgSwitchOrdersPage ());
-			}
-			else
-			{
-				// Driver does not yet have a car assigned.
-				CmdGetAvailableCars getAvailableCars = new CmdGetAvailableCars ();
-				connection.Send (getAvailableCars);
+                Messenger.Default.Send<MsgSwitchOrdersPage>(new MsgSwitchOrdersPage());
+            }
+            else
+            {
+                // Driver does not yet have a car assigned.
+                CmdGetAvailableCars getAvailableCars = new CmdGetAvailableCars();
+                connection.Send(getAvailableCars);
 
-				Messenger.Default.Send<MsgSwitchSelectCarPage> (new MsgSwitchSelectCarPage ());
-			}
-		}
+                Messenger.Default.Send<MsgSwitchSelectCarPage>(new MsgSwitchSelectCarPage());
+            }
+        }
 
-	}
+    }
 }
